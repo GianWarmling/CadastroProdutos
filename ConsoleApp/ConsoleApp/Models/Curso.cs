5dda26ccbe602cd74931e0e5230d7c6fe46b2911
Gian Warmling <130003869+GianWarmling@users.noreply.github.com>
2024-10-23 20:54:10 -0300

    criado model curso

--- ConsoleApp/ConsoleApp/Models/Curso.cs
+++ ConsoleApp/ConsoleApp/Models/Curso.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp.Models
 {
-    internal class Curso
+    public class Curso
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Id}; {this.Nome}; {this.Descricao}; {this.Valor}";
+        }
     }
 }

133a36b5d94f0f52e8ed4e2fd6b3c266aea15adf
Gian Warmling <130003869+GianWarmling@users.noreply.github.com>
2024-10-23 21:09:35 -0300

    criado controller curso

--- ConsoleApp/ConsoleApp/Controllers/CursoController.cs
+++ ConsoleApp/ConsoleApp/Controllers/CursoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,37 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp.Controllers
 {
-    internal class CursoController
+    public class CursoController
     {
+        private static int _id = 0;
+
+        private static List<Curso> _listaCursos = new List<Curso>();
+
+        public void Create(Curso model)
+        {
+            _id += 1;
+            model.Id = _id;
+            _listaCursos.Add(model);
+        }
+        public void Update(Curso model) 
+        {
+            Curso modelOld = GetById(model.Id);
+            int index = _listaCursos.IndexOf(modelOld);
+            _listaCursos[index] = model;
+        }
+        public void Delete(int id)
+        {
+            Curso model = GetById(id);
+            _listaCursos.Remove(model);
+        }
+        public List<Curso> GetAll() 
+        {
+            return _listaCursos;
+        }
+        public Curso GetById(int id) 
+        {
+            Curso model = _listaCursos.Find(c => c.Id == id);
+            return model;
+        }
     }
 }
